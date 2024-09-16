5db8a564d6ed7cb13fc22680ddbc8b840e75cb2a
Ismael Farias <125519671+ismaelfgomes@users.noreply.github.com>
2024-09-16 20:13:41 -0300

    Adicionada função para movimentação da task no board

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ const taskModalContent = document.querySelector("#modal-content-form");
 const closeTaskModal = document.getElementsByClassName("close")[0];
 const taskModalForm = document.getElementsByClassName("taskEditForm")
 
+// Cunsulta tarefas existentes no banco de dados e apresenta no Board
 const getList = async () => {
     let url = 'http://127.0.0.1:5000/tasks';
     fetch(url, {
@@ -20,7 +21,6 @@ const getList = async () => {
     })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             data.tasks.forEach(item => saveTask(item.titulo, item.id, item.status));
 
         })
@@ -51,34 +51,71 @@ const addTask = async (text) => {
         });
 }
 
-const editTask = async (id) => {
+const updateTask = () => {
+    let taskID = document.getElementsById("taskID").value;
+    let taskTitulo = document.getElementById("task-titulo").value;
+    let taskDescricao = document.getElementById("itemDesc").value;
+
+    const formData = new FormData();
+    formData.append("id", taskID);
+    formData.append("titulo", taskTitulo);
+    formData.append("descricao", taskDescricao)
     let url = 'http://127.0.0.1:5000/task';
-    fetch(url  + "?id=" + id, {
-        method: 'GET',
+    console.log(formData);
+    fetch(url, {
+        method: 'PATCH',
+        body: formData
     })
-//        .then((response) => response.json())
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            return Promise.reject(response);
-        })
-        .then((data) => {
-            console.log(data);
-            data.tasks.forEach(item => (tid = item.id, titulo = item.titulo, descricao = item.descricao, taskstatus = item.status, dt_criacao = item.dt_criacao));
-            const taskEdit = document.createElement("form");
+        .then((response) => updateStats())
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function updateStats() {
+    alert("Tarefa Atualizada")
+    taskModal.style.display = "none";
+    taskModalContent.replaceChildren();
+}
+
+
+const editTask = async (id, titulo) => {
+    let url = 'http://127.0.0.1:5000/task';
+
+    const taskEdit = document.createElement("form");
             taskEdit.setAttribute('class', "taskEditForm")
             taskEdit.setAttribute('id', "taskEditForm")
             const formItemID = document.createElement("input");
             formItemID.setAttribute('class', "taskID");
             formItemID.setAttribute('id', "taskID");
-            formItemID.setAttribute('value', tid)
+            formItemID.setAttribute('value', id)
             taskEdit.appendChild(formItemID);
-
             const formtItemTitulo = document.createElement("input");
             formtItemTitulo.setAttribute('value', titulo)
             formtItemTitulo.setAttribute('id', "task-titulo")
             taskEdit.appendChild(formtItemTitulo)
+            taskModalContent.appendChild(taskEdit);
+
+    fetch(url + "?id=" + id, {
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            data.tasks.forEach(item => (tid = item.id, titulo = item.titulo, descricao = item.descricao, taskstatus = item.status, dt_criacao = item.dt_criacao));
+            // const taskEdit = document.createElement("div");
+            // taskEdit.setAttribute('class', "taskEditForm")
+            // taskEdit.setAttribute('id', "taskEditForm")
+            // const formItemID = document.createElement("input");
+            // formItemID.setAttribute('class', "taskID");
+            // formItemID.setAttribute('id', "taskID");
+            // formItemID.setAttribute('value', tid)
+            // taskEdit.appendChild(formItemID);
+
+            // const formtItemTitulo = document.createElement("input");
+            // formtItemTitulo.setAttribute('value', titulo)
+            // formtItemTitulo.setAttribute('id', "task-titulo")
+            // taskEdit.appendChild(formtItemTitulo)
 
             const formtItemDesc = document.createElement("input");
             formtItemDesc.setAttribute('value', descricao)
@@ -97,8 +134,10 @@ const editTask = async (id) => {
 
             const saveBtn = document.createElement("button");
             saveBtn.setAttribute('id', "save-btn")
+            saveBtn.setAttribute('value', "salvar")
             saveBtn.classList.add("saveInput");
-            saveBtn.innerHTML = 'Salvar. <i class="fa-solid fa-check"></i>';
+            saveBtn.setAttribute('onclick', "updateTask()")
+            saveBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
             taskEdit.appendChild(saveBtn);
 
             console.log(taskEdit)
@@ -109,7 +148,10 @@ const editTask = async (id) => {
         .catch((error) => {
             console.error('Error:', error);
         });
-
+//         .finally(() => {
+//         alert("Servidor indisponivel")
+//         updateStats()
+//    });
 }
 
 const delTask = async (id) => {
@@ -133,24 +175,35 @@ const saveTask = (text, id = 1, status) => {
     tasks.appendChild(newItemID);
 
     const newItemTitle = document.createElement("h4");
+    newItemTitle.setAttribute('class', "taskTitulo")
     newItemTitle.innerText = text;
     tasks.appendChild(newItemTitle);
 
     const editBtn = document.createElement("button");
     editBtn.classList.add("edit-task");
-    editBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
     tasks.appendChild(editBtn);
+
+    const updateBtn = document.createElement("button");
+    updateBtn.classList.add("init-task");
+    updateBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+    tasks.appendChild(updateBtn);
 
     const delBtn = document.createElement("button");
     delBtn.classList.add("delInput");
     delBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     tasks.appendChild(delBtn);
 
+    const taskStatus = document.createElement("span");
+    taskStatus.classList.add("task-status");
+    taskStatus.innerText = status;
+    tasks.appendChild(taskStatus);
+
     if (status == 1) {
         newItems.appendChild(tasks);
     }
     else if (status == 2) {
-        editBtn.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+        updateBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
         doingTasks.appendChild(tasks);
 
     }
@@ -159,6 +212,8 @@ const saveTask = (text, id = 1, status) => {
 
     }
     else {
+        taskStatus.innerText = 1;
+        tasks.appendChild(taskStatus);
         newItems.appendChild(tasks);
     }
 
@@ -167,6 +222,7 @@ const saveTask = (text, id = 1, status) => {
     newTaskInput.focus();
 
 }
+
 
 // Eventos
 
@@ -185,24 +241,45 @@ newTaskForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetEl = e.target;
     const parentEl = targetEl.closest("div");
+    const taskID = parentEl.querySelector(".taskID").innerText;
 
     if (targetEl.classList.contains("edit-task")) {
-        const taskID = parentEl.querySelector(".taskID").innerText;
+
+        const taskTitulo = parentEl.querySelector(".taskTitulo").innerHTML;
         const source = parentEl;
-        doingTasks.append(source);
         taskModal.style.display = "block";
-        editTask(taskID);
+        editTask(taskID, taskTitulo);
+    }
+    else if (targetEl.classList.contains("init-task")) {
+        const source = parentEl
+        const taskStatus =  parentEl.querySelector(".task-status");
+        if (taskStatus.innerText == 1) {
+            taskStatus.innerText = 2;
+            console.log(taskStatus)
+            console.log(taskID);
+            doingTasks.append(source);
+        } else {
+            taskStatus.innerText = 3;
+            doneTasks.append(source);
+        }
+        
     }
 
-    if (targetEl.classList.contains("delInput")) {
+    else if (targetEl.classList.contains("delInput")) {
         parentEl.remove();
         console.log(taskID);
         delTask(taskID);
     }
 
-    if (targetEl.classList.contains("close")) {
+    else if (targetEl.classList.contains("close-task")) {
+        parentEl.remove();
+        console.log(taskID);
+        delTask(taskID);
+    }
+
+    else if (targetEl.classList.contains("close")) {
         taskModal.style.display = "none";
         taskModalContent.replaceChildren();
-        
+
     }
 })
